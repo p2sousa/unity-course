2885d1c53b6c6cc9387da30c2cec8c95f6ccd098
Pablo Sousa <pablosousa.ads@gmail.com>
2019-05-21 22:14:11 -0300

    UnityCSharpAdvanced: aula 01 - 02

---
+++ UnityCSharpAdvanced/Assets/awakVsStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class awakVsStart : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Awak Executada!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        Debug.Log("Start Executada!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

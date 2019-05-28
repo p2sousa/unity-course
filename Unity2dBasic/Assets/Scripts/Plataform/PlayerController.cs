e517f946d6dcd38366938a1128e6a595e89f218e
Pablo Sousa <pablosousa.ads@gmail.com>
2019-05-27 22:25:54 -0300

    Unity2dBasic: aula 07 - aula 10

---
+++ Unity2dBasic/Assets/Scripts/Plataform/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float velocidade = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 movimento = new Vector2(horizontal, 0);
+
+        rb.MovePosition(rb.position + (movimento * velocidade) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
